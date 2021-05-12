3766e7b02629d27ae447f6c1980c74022c2adecf
WanDv <>
2021-05-12 17:59:22 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let webBrowsers = ['Internet Explorer', 'Vivaldi', 'Firefox', 'Chrome', 'Opera', 'Edge']
+
+console.log(webBrowsers.shift())
+console.log(webBrowsers.pop())
+console.log(webBrowsers.length)
+console.log(webBrowsers.slice(1, 3))
+console.log(webBrowsers)
